1ca052bb96bae40c08f8d8f715434625a450f86f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 21:32:33 +0200

    added functionality for multiple ports

--- src/Client.java
+++ src/Client.java
@@ -2,20 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
 
 public class Client {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private final int[] listOfFreePorts = {4002, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010, 4011};
+    private final String IP = "127.0.0.1", SERVER_RECOGNITION_MESSAGE = "abcd";
 
-    public Client(String ip, int port) {
-        try {
-            clientSocket = new Socket(ip, port);
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Client() throws ConnectException {
+        boolean serverFound = false;
+        for (int port : listOfFreePorts) {
+            try {
+                clientSocket = new Socket(IP, port);
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                if (!SERVER_RECOGNITION_MESSAGE.equals(in.readLine())) {
+                    clientSocket.close();
+                    out.close();
+                    in.close();
+                    continue;
+                }
+                out.println("found you!");
+                serverFound = true;
+                break;
+            } catch (IOException e) {
+                 System.out.printf("Port %d occupied, trying next one\n", port);
+            }
+        }
+        if (!serverFound) {
+            throw new ConnectException("No server found!");
         }
     }
 
@@ -37,8 +55,8 @@ public class Client {
 
     public static void main(String[] args) {
         if (args.length > 0) {
-            Client client = new Client("127.0.0.1", 6666);
             try {
+                Client client = new Client();
                 client.sendMessage(args[0]);
             } catch (IOException e) {
                 e.printStackTrace();
--- src/Server.java
+++ src/Server.java
@@ -2,14 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.BindException;
+import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server extends Thread {
     private ServerSocket serverSocket;
+    private final int[] listOfFreePorts = {4002, 4003, 4004, 4005, 4006, 4007, 4008, 4009, 4010, 4011};
 
-    public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+    public Server() throws ConnectException {
+        boolean portFound = false;
+        for (int port : listOfFreePorts) {
+            try {
+                serverSocket = new ServerSocket(port);
+                portFound = true;
+                break;
+            } catch (BindException e) {
+                System.out.printf("Port %d occupied, trying next one\n", port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!portFound) {
+            throw new ConnectException("No free port found!");
+        }
+        System.out.println("Connection established!");
+    }
+
+    public void run() {
         while (!serverSocket.isClosed()) {
             try {
                 new SocketHandler(serverSocket.accept()).start();
@@ -19,13 +40,13 @@ public class Server {
         }
     }
 
-    public void stop() throws IOException {
+    public void exit() throws IOException {
         serverSocket.close();
     }
     public static void main(String[] args) {
-        Server server=new Server();
         try {
-            server.start(6666);
+            Server server=new Server();
+            server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,11 +56,19 @@ public class Server {
         private final Socket socket;
         private final PrintWriter out;
         private final BufferedReader in;
+        private final String recognizeMeMessage = "abcd"; //open to suggestions
 
         public SocketHandler(Socket s) throws IOException {
             socket = s;
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.println(recognizeMeMessage);
+            if (!"found you!".equals(in.readLine())) {
+                s.close();
+                out.close();
+                in.close();
+                throw new ConnectException("Client did not respond");
+            }
         }
 
         @Override
