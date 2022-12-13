28de1cda3ab3379f2632a00385e147bb7c136e40
MarekDinka <dinka2@uniba.sk>
2022-12-13 15:03:02 +0100

    fix

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -3,7 +3,7 @@ import sk.uniba.fmph.Server;
 
 public class Main {
     public static void main(String[] args) {
-        Server.getInstance().begin();
         ControllerCommunicationHandler.getInstance().requestControllerIps();
+        Server.getInstance().begin();
     }
 }
