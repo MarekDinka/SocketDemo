91e47a1228ce2a5a67e52588c802c40eea7e7fd3
MarekDinka <dinka2@uniba.sk>
2022-12-25 13:20:57 +0100

    a

---
+++ Client/src/Burnie/Main.java
@@ -0,0 +1,18 @@
+package Burnie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            ClientHandler ch = new ClientHandler();
+            while (true) {
+                System.out.println("Hallo:");
+                System.out.println(ch.getNumberOfProjects());
+                System.out.println(ch.getNumberOfControllers());
+                Thread.sleep(5000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
